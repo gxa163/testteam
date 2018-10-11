f136ce2104bdc43400bc1dab81f5d4d58fcf1590
gotoshop11 <43644566+gotoshop11@users.noreply.github.com>
2018-10-11 17:05:44 +0800

    hah

---
+++ mybatis1_2/test/test/mybatis1_1/Longtest.java
@@ -0,0 +1,11 @@
+package test.mybatis1_1;
+
+public class Longtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Long idLong=1L;
+		System.out.println(idLong);
+	}
+
+}
